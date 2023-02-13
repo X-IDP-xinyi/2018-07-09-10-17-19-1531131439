b2ec881b33b9baae233271a8a867ac9a59c2b845
Andrea <Andrea2000728>
2023-02-13 21:35:29 +0800

    feat: implement Person Student Teacher Klass class and pass the test

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,60 @@
 package practice11;
 
 public class Klass {
+    private final int number;
+    private Student leader;
+    private Teacher teacher;
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().number != this.number) {
+            System.out.print("It is not one of us.\n");
+        }else {
+            setLeader(student);
+        }
+        if (null != this.getTeacher()) {
+            if (student.getKlass().number == this.number) {
+                System.out.print("I am " + this.getTeacher().getName() + ". I know " + student.getName()
+                        + " become Leader of Class " + this.getNumber() + ".\n");
+            }
+        }
+    }
+    public void appendMember(Student student){
+        student.setKlass(this);
+        if (null != this.getTeacher()) {
+            System.out.print("I am " + this.getTeacher().getName() + ". I know " + student.getName()
+                    + " has joined Class " + this.getNumber() + ".\n");
+        }
+    }
+
+    public boolean isIn(Student student) {
+        return this.number == student.getKlass().number;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,70 @@
 package practice11;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes = new LinkedList<>();
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for (Klass klass : classes) {
+            klass.setTeacher(this);
+        }
+    }
+
+    @Override
+    public String introduce() {
+        if (classes.isEmpty()) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            List<String> teachClass = new ArrayList<>();
+            for (Klass aClass : classes) {
+                teachClass.add(String.valueOf(aClass.getNumber()));
+            }
+            return super.introduce() + " I am a Teacher. I teach Class " + String.join(", ", teachClass) + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        for (Klass aClass : classes) {
+            if (student.getKlass().getNumber() == aClass.getNumber()) {
+                return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+            } else {
+                return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Teacher teacher = (Teacher) o;
+        return Objects.equals(classes, teacher.classes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), classes);
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass aClass : classes)
+            return aClass.isIn(student);
+        return false;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
 }
