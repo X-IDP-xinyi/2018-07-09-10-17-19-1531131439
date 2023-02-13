98f02fd63ddab768cb809eebf2406de827581742
Andrea <Andrea2000728>
2023-02-13 13:51:09 +0800

    feat: implement Person Student Teacher Klass class and pass the test

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,43 @@
 package practice10;
 
-public class Student {
+import java.util.Objects;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass.getLeader() == this) {
+            return super.introduce() + " I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        } else {
+            return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+        }
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return Objects.equals(klass, student.klass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), klass);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,66 @@
 package practice10;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Teacher extends Person {
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if (classes != null) {
+            List<String> teachClass = new ArrayList<>();
+            for (Klass aClass : classes) {
+                teachClass.add(String.valueOf(aClass.getNumber()));
+            }
+            return super.introduce() + " I am a Teacher. I teach Class " + String.join(", ", teachClass) + ".";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        for (Klass aClass : classes) {
+            if (student.getKlass().getNumber() == aClass.getNumber()) {
+                return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+            } else {
+                return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Teacher teacher = (Teacher) o;
+        return Objects.equals(classes, teacher.classes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), classes);
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass aClass : classes)
+            return aClass.isIn(student);
+        return false;
+    }
 }
